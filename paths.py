29eb771486a917cbf441394bfb5ab91ecd19b5a3
sshleifer <sshleifer@gmail.com>
2019-07-22 20:22:06 -0700

    options pkl

--- paths.py
+++ paths.py
@@ -8,7 +8,7 @@ DATA_DIR = HOME/'MovieTriples/'
 TEST_TRIPLES_PATH = DATA_DIR / 'Test.triples.pkl'
 #OLD_VAL_TRIPLES_PATH = DATA_DIR / 'Validation.triples.pkl'
 #OLD_TRAIN_TRIPLES_PATH = DATA_DIR + '/Training.triples.pkl'
-training_dict_path = DATA_DIR + 'Training.dict.pkl'
+training_dict_path = DATA_DIR / 'Training.dict.pkl'
 eval_source_path = '/data2/chatbot_eval_issues/results/AMT_NCM_Test_NCM_Joao/neural_conv_model_eval_source.txt'
 dict_file = '/home/harshals/hed-dlg/Data/MovieTriples/Training.dict.pkl'
 
